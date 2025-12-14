a452b576f60d5b0c1ad6faafb28f0d16de2f6543
Ravindu Indranath <indrana-ps21164@stu.kln.ac.lk>
2025-12-14 18:38:58 +0530

    delete extra files.

--- Back-End-Transpo-main/src/main/java/com/Transpo/transpo/repository/BusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Transpo.transpo.repository;
-
-import com.Transpo.transpo.model.Bus;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BusRepository extends MongoRepository<Bus, String> {
-
-}
