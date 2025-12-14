61ca0919f77e6b0bd78920e4a11df3e53bf38291
Ravindu Indranath <indrana-ps21164@stu.kln.ac.lk>
2025-12-14 16:44:41 +0530

    create ReservationRepository

--- src/main/java/com/Transpo/transpo/repository/ReservationRepository.java
+++ src/main/java/com/Transpo/transpo/repository/ReservationRepository.java
@@ -1,12 +1,12 @@
 package com.Transpo.transpo.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.Transpo.transpo.model.Reservation;
 import java.util.List;
 
-public interface ReservationRepository extends MongoRepository<Reservation, String> {
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     List<Reservation> findByPassengerEmail(String email);
-
-    List<Reservation> findByScheduleId(String scheduleId);
+    List<Reservation> findByScheduleId(Long scheduleId);
 }
